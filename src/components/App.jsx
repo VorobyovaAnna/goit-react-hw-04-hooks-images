69421faab4f25cc339729adb19cb916a9ab88fdf
VorobyovaAnna <anyasonn1k@gmail.com>
2022-05-10 22:25:04 +0300

    added image finder made on react-classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import api from '../services/api-service';
+import Button from './Button';
+import Modal from './Modal';
+import Loader from './Loader';
+
+
+
+export default class App extends Component {
+  state = {
+    images: [],
+    largeImage: '',
+    alt: '',
+    searchName: '',
+    page: 1,
+    isModalOpen: false,
+    isLoading: false,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchName !== this.state.searchName || prevState.page !== this.state.page) {
+      // console.log('prevState.searchName:', prevState.searchName);
+      // console.log('this.state.searchName:', this.state.searchName);
+      // console.log('Изменился запрос');
+      
+      api
+        .getImages(this.state.searchName, this.state.page)
+        .then(images => this.setState(prevState => ({ images: [...prevState.images, ...images] })))
+        .catch(console.log)
+        .finally(() => this.setState({ isLoading: false }));
+    }
+  }
+
+  handleFormSubmit = searchName => {
+    if (searchName === this.state.searchName) return;
+    this.setState({ searchName: searchName, page: 1, images: [], isModalOpen: false, isLoading: true });
+  }
+  onLoadMore = () => {
+    this.setState(() => ({ page: this.state.page + 1, isLoading: true }));
+  };
+
+  onOpenModal = id => this.setState(prevState => ({
+    isModalOpen: true,
+    largeImage: prevState.images.find(image => image.id === id).largeImageURL,
+    alt: prevState.images.find(image => image.id === id).tags,
+  }));
+
+  onCloseModal = () => this.setState({ isModalOpen: false });
+
+
+  render() {
+    const { images, largeImage, alt, isModalOpen, isLoading } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery images={images} onOpenModal={this.onOpenModal}/>
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && <Button onClick={this.onLoadMore} />}
+        {isModalOpen && <Modal largeImageURL={largeImage} alt={alt} onCloseModal={this.onCloseModal} />}
+        <ToastContainer autoClose={3000} />
+
+
+      </div>
+     
+   );
+ }
+}
